c7af77885f5bb8b06a3ab762b227bc4bd9a45e02
murilomorano3 <133981366+murilomorano3@users.noreply.github.com>
2024-10-20 18:09:37 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    "Programção",
+    "O que é Python?",
+    "O Python é uma linguagem de programação"
+)
+
+criaCartao(
+    "Geografia",
+    "Qual a capital da França?",
+    "A capital da França é Paris"
+)
+
+criaCartao(
+    "Programção",
+    "O que é uma função?",
+    "Uma função é um bloco de códgigo que executa alguma tarefa"
+)
+
+criaCartao (
+    "Lingua Inglesa",
+    "Como se diz oi em Inglês?",
+    "Oi em Inglês é HI (RAI)"
+)
